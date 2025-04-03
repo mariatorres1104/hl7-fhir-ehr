11be509765c28e668f23245802406cc44b380252
mariatorres1104 <mafytorresabril@hotmail.com>
2025-04-03 00:11:15 -0500

    cambios

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Request
 import uvicorn
-from app.controlador.PatientCrud import GetPatientById,WritePatient
+from app.controlador.PatientCrud import GetPatientById,WritePatient, GetPatientByIdentifier
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -13,13 +13,13 @@ app.add_middleware(
     allow_headers=["*"],  # Permitir todos los encabezados
 )
 
-@app.get("/patient/{patient_id}", response_model=dict)
-async def get_patient_by_id(patient_id: str):
-    status,patient = GetPatientById(patient_id)
+@app.get("/patient", response_model=dict)
+async def get_patient_by_identifier (system: str, value: str):
+    status,patient = GetPatientByIdentifier(system, value)
     if status=='success':
         return patient  # Return patient
     elif status=='notFound':
-        raise HTTPException(status_code=404, detail="Patient not found")
+        raise HTTPException(status_code=204, detail="Patient not found")
     else:
         raise HTTPException(status_code=500, detail=f"Internal error. {status}")
 
